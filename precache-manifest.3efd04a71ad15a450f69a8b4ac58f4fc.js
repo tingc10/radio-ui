ccb80505589d0cab636ac66fd753c4968ac99c76
Ting Chen <ting@squareup.com>
2020-06-07 20:19:29 -0400

    Updates

---
+++ precache-manifest.3efd04a71ad15a450f69a8b4ac58f4fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e464a3dc309b66bdf379cf2e47f2e3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5814f2d0ff84ce3c5f7d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82b256b469f17d7a9103",
+    "url": "/static/js/2.5efaaa46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5efaaa46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5814f2d0ff84ce3c5f7d",
+    "url": "/static/js/main.faea2c1d.chunk.js"
+  },
+  {
+    "revision": "8f27b2408bff94e27b8c",
+    "url": "/static/js/runtime-main.2d7d5397.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
