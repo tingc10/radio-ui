50bd939240a044706bb28ff8ccd759b7e759d784
Ting Chen <ting@squareup.com>
2020-06-07 21:37:23 -0400

    Updates

---
+++ precache-manifest.b7ec5dfa6aee6cb9520e752f254fcc61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e29740a853b0e7947fd4922bff92f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "250f0e9f9647522e7309",
+    "url": "/static/css/main.77de209a.chunk.css"
+  },
+  {
+    "revision": "23c0906b1e51f0dceb79",
+    "url": "/static/js/2.f35ff41a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f35ff41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250f0e9f9647522e7309",
+    "url": "/static/js/main.601bc85d.chunk.js"
+  },
+  {
+    "revision": "8f27b2408bff94e27b8c",
+    "url": "/static/js/runtime-main.2d7d5397.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3efd04a71ad15a450f69a8b4ac58f4fc.js"
+  "/precache-manifest.b7ec5dfa6aee6cb9520e752f254fcc61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
+
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
